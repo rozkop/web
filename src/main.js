ad423fc9680249e6e92261bf4fe99d5365295bdf
vojcc <kuba.wojcik000@gmail.com>
2023-05-15 00:16:55 +0200

    rollback

--- src/main.js
+++ src/main.js
@@ -8,8 +8,7 @@ import { createPinia } from 'pinia';
 import router from './router';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://a9b2-178-43-138-218.ngrok-free.app'
-axios.defaults.headers.common['ngrok-skip-browser-warning'] = true;
+axios.defaults.baseURL = 'http://localhost:8000'
 
 const token = $cookies.get('auth_token');
 if (token) {
